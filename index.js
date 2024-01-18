8b800ed92c1b6f172f13aa8fa04e2c4d0992690f
sakibZeem <anamulsakibzim22@gmail.com>
2024-01-18 23:16:18 +0600

    basic newspaper server for categories

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+const categories = require('./data/categories.json')
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('News Dragon Running');
+})
+
+app.get('/categories', (req, res) => {
+  res.send(categories);
+})
+app.listen(port, () => {
+  console.log(`News Dragon API is running on port ${port}`)
+})
